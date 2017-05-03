6a078587e218d1e90d4dfd43191e6af7d941ca52
Levie Abigail Lutero <ayellutero@gmail.com>
2017-05-03 16:40:13 +0800

    Added schedules table Modified CalendarController & Schedule model Modified migration

--- resources/views/Calendar/index.blade.php
+++ resources/views/Calendar/index.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+
+@if(Session::has('message'))
+<div class="alert alert-success alert-dismissable">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  {{ Session::get('message') }}
+</div>
+
+@endif
+
 <div id="full-calendar" class="row card-panel" style="margin:1.5%; margin-top:0%; text-align: center;">
     <div class="divider"></div><br>
     <div class="col s11 m7 l6">
@@ -8,7 +17,7 @@
     </div>
 
     <div>
-        <a class="btn btn-success" data-toggle="modal" data-target="#createSched" onclick="createSched()"><i class="fa fa-plus" aria-hidden="true"></i> Create New Schedule</a>
+        <a class="btn btn-success" data-toggle="modal" data-target="#createSched"><i class="fa fa-plus" aria-hidden="true"></i> Create New Schedule</a>
     </div>
 
     <!-- View Report Modal -->
@@ -18,17 +27,48 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">New Sched</h4>
+                    <h4 class="modal-title">Schedule New Maintenance</h4>
                 </div>
                 
-                <div class="modal-body">
-                        1. When <br>
-                        2. Station <br>
-                        3. Staff-in-charge <br>
-                        4. Notify via text/email every ____
+                <div class="modal-body" style="text-align: left">
+                    {!! Form::open(['url' => 'calendar']) !!}
+                        <div class="form-group hide">
+                            {!! Form::text('title', 'Station maintenance',['class'=>'form-control', 'required' => 'true']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('station_label', 'Station Name:') !!}
+                            {!! Form::text('station', null,['class'=>'form-control', 'required' => 'true']) !!}
+	                    </div>
+
+                        <div class="form-group">
+                            {!! Form::label('start_date_label', 'Date:') !!}
+                            <input type="date" name="start_date" class="datepicker" required>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('staff-in-charge', 'Staff-in-Charge:') !!}
+                            {!! Form::text('staff', null,['class'=>'form-control', 'required' => 'true']) !!}
+	                    </div>
+
+                        <div class="form-group">
+                            {!! Form::label('notify-via', 'Notify via:') !!}
+                            <br>
+                            <div id="notify_email">
+                                {{ Form::checkbox('notify_email', true) }} E-mail
+                            </div>
+                            {!! Form::text('email_to_notif', null,['id' => 'email_notif', 'class'=>'form-control']) !!}
+                            <br>
+                            <div id="notify_sms">
+                                {{ Form::checkbox('notify_sms', 'true') }} SMS
+                            </div>
+                            {!! Form::text('sms_to_notif', null,['id' => 'sms_notif', 'class'=>'form-control']) !!}
+                            
+	                    </div>
                 </div>
                 <div class="modal-footer">
                     <button   id="button_id" class="btn btn-success" type="submit" name="action">Create</button>
+                     {{ Form::close() }}
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                 </div>
             </div>
@@ -41,6 +81,7 @@
 <script>
 
 function createSched(){
+    alert('HELLO');
    /* var date = new Date();
         var d = date.getDate();
         var m = date.getMonth();
@@ -71,20 +112,6 @@ function createSched(){
 
         $('#calendar').fullCalendar( 'renderEvent', $event , 'stick');*/
 }
-/*
-        var date = new Date();
-        var d = date.getDate();
-        var m = date.getMonth();
-        var y = date.getFullYear();
 
-       $('#button_id').click(function() {
-            var newEvent = {
-                title: 'NEW EVENT',
-                start: new Date(y, m, d)
-            };
-            $('#calendar').fullCalendar( 'renderEvent', newEvent , 'stick');
-        });
-
-    */
 </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -221,6 +221,16 @@
            $('#change_pw').hide();
         });
 
+        // hide notify-via fields
+        $('#notify_email').change(function () {                
+            $('#email_notif').toggle(this.unchecked);
+        }).change();
+
+        $('#notify_sms').change(function () {                
+            $('#sms_notif').toggle(this.unchecked);
+        }).change();
+       
+
         // Datatable JS
         $('#all-users').DataTable();
         $('#all-reports').DataTable();
@@ -228,80 +238,7 @@
 
         //tooltip
         $('[data-toggle="tooltip"]').tooltip(); 
-        /*
-        var date = new Date();
-        var d = date.getDate();
-        var m = date.getMonth();
-        var y = date.getFullYear();
-
-       $('#button_id').click(function() {
-            var newEvent = {
-                title: 'NEW EVENT',
-                start: new Date(y, m, d)
-            };
-            $('#calendar').fullCalendar( 'renderEvent', newEvent , 'stick');
-        });
-
-    */
-   /* var events = {!! $events !!}
-    // foreach(events as event){
-        $('#button_id').click(function() {
-        var newEvent = {
-                title: events[0].title,
-                start: events[0].date
-        }
-        $('#calendar').fullCalendar('renderEvent',newEvent,true)
-    // }
-    });
-
-    // alert( events[0].title); */
-
-       
-
-    /**************/
-    /*
-            var calendar = $('#calendar').fullCalendar({
-            defaultView: 'agendaWeek',
-            editable: true,
-                selectable: true,
-            //header and other values
-            select: function(start, end, allDay) {
-                endtime = $.fullCalendar.formatDate(end,'h:mm tt');
-                starttime = $.fullCalendar.formatDate(start,'ddd, MMM d, h:mm tt');
-                var mywhen = starttime + ' - ' + endtime;
-                $('#createEventModal #apptStartTime').val(start);
-                $('#createEventModal #apptEndTime').val(end);
-                $('#createEventModal #apptAllDay').val(allDay);
-                $('#createEventModal #when').text(mywhen);
-                $('#createEventModal').modal('show');
-            }
-            });
-
-        $('#submitButton').on('click', function(e){
-            // We don't want this to act as a link so cancel the link action
-            e.preventDefault();
-
-            doSubmit();
-        });
-
-        function doSubmit(){
-            $("#createEventModal").modal('hide');
-            console.log($('#apptStartTime').val());
-            console.log($('#apptEndTime').val());
-            console.log($('#apptAllDay').val());
-            alert("form submitted");
-                
-            $("#calendar").fullCalendar('renderEvent',
-                {
-                    title: $('#patientName').val(),
-                    start: new Date($('#apptStartTime').val()),
-                    end: new Date($('#apptEndTime').val()),
-                    allDay: ($('#apptAllDay').val() == "true"),
-                },
-                true);
-        }
         
-    */
     });
 
 
