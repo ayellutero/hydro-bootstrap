dc229fc31b1b284609b74871ac5bb064aac072b7
Abs Mendoza <absmendoza>
2017-05-18 22:55:46 +0800

    Modified GMap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -115,7 +115,6 @@
     </div>
 </div>
 
-<!---->
 <div id="all-stat-data" class="hide">
     {{ $statsData }}
 </div>
@@ -145,8 +144,9 @@
                 infoWindow: {
                     content:'<b>Device ID: </b>' + value.device_id + '</br><b>Location: </b>' 
                             + value.location + ' ' + value.province 
-                            + '<br><b>Latitude: </b>' + value.lat + '<b> Longitude: </b>' + value.lng + '<br><b>Status: </b>' + value.status + '<br><b>Elevation: </b>' + value.elevation
-                            + '<br><b>Type: </b>' + value.type + '<br>'
+                            + '<br><b>Latitude: </b>' + value.lat + '<b> Longitude: </b>' + value.lng + '<b> Elevation: </b>' + value.elevation
+                            + '<br><b>Type: </b>' + value.type + '<b> Sim Network: </b>' + value.sim
+                            + '<br><b>Status: </b>' + value.status 
                 },
                 click: function(e) {
                     infoWindow.open();
